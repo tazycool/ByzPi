441a97edf93a66016545451b9b99ad6e0ce37b8f
root <root@raspberrypi.byzantium.org>
2013-04-04 04:27:19 +0000

    got puppet modules working - still incomplete - progress on etherpad-lite

--- puppet-etc/manifests/site.pp
+++ puppet-etc/manifests/site.pp
@@ -1,28 +1,30 @@
-file { '/etc/captiveportal':
-  ensure => 'directory',
-}
-file { '/etc/captiveportal/captiveportal.conf':
-  ensure      => 'file',
-  mode        => '0644',
-  content     => '[/]
-tools.staticdir.root = "/srv/captiveportal"
-
-',
-  subscribe   => File['/etc/captiveportal'],
-}
-file { '/srv/captiveportal/':
-  ensure      => 'directory',
-  mode        => '0644',
-}
-file { '/srv/captiveportal/':
-  source => 'puppet://srv-captiveportal',
-  recurse => true,
-}
-
 # Nodejs for etherpad
 
-package { [libc-ares2' 'libc6', 'libev4', 'libgcc1', 'libssl1.0.0',
-  'libstdc++6', 'libv8-3.8.9.20', 'zlib1g']
+package { ['libc-ares2','libc6','libev4','libgcc1','libssl1.0.0','libstdc++6',
+  'libv8-3.8.9.20','zlib1g','libmysql-ruby','libjson-ruby', 'ruby-json',
+  'rubygems']:
+  ensure     =>   'present',
+}
+package { ['apt-file','git','mlocate']:
   ensure     =>   'present',
 }
 class { 'nodejs': }
+exec { 'gem install etherpad-lite':
+  path       =>   '/bin:/usr/bin',
+  onlyif     =>   'gem list --local |grep etherpad-lite|wc -l |grep 0 > /dev/null',
+}
+file { '/etc/hosts':
+  content     =>   '127.0.0.1	localhost
+::1		localhost ip6-localhost ip6-loopback
+fe00::0		ip6-localnet
+ff00::0		ip6-mcastprefix
+ff02::1		ip6-allnodes
+ff02::2		ip6-allrouters
+
+127.0.1.1	raspberrypi raspberrypi.byzantium.org
+',
+}
+file { '/etc/hostname':
+  content     =>   'raspberrypi.byzantium.org
+',
+}
--- puppet-etc/modules/etherpad-lite/manifests/init.pp
+++ puppet-etc/modules/etherpad-lite/manifests/init.pp
@@ -1,8 +1,17 @@
 class etherpad_lite (
-  $db_name = 'etherpad-lite', $db_user = 'etherpad-lite', $db_host = '127.0.0.1', $db_password = 'absent',
-  $nodejs_version = 'installed', $settings_content = 'absent', $settings_source = 'absent',
-  $repo = 'git://github.com/Pita/etherpad-lite.git', $repo_ensure = 'present', $repo_rev = 'master',
-  $ensure = 'present', $clear_pads = 'absent', $api_url = 'absent', $api_key = 'absent'
+  $db_name = 'etherpad-lite',
+  $db_user = 'etherpad-lite',
+  $db_host = '127.0.0.1',
+  $db_password = 'absent',
+  $nodejs_version = 'installed',
+  $settings_content = 'absent',
+  $settings_source = 'absent',
+  $repo = 'git://github.com/Pita/etherpad-lite.git',
+  $repo_ensure = 'present', $repo_rev = 'master',
+  $ensure = 'present',
+  $clear_pads = 'absent',
+  $api_url = 'absent',
+  $api_key = 'absent'
 ) {
 
   # Latest version of nodejs (0.6.3+) has npm baked-in, so we dont install the package
@@ -11,8 +20,8 @@ class etherpad_lite (
 
   Package['libaugeas-ruby'] -> Augeas <| |>
 
-  package { [ "abiword", "curl", "wget", 'libaugeas-ruby' ]:
-    ensure => installed;
+  package { [ 'abiword', 'curl', 'wget', 'libaugeas-ruby' ]:
+    ensure => 'installed',
   }
 
 # Added by class nodejs #
@@ -40,18 +49,23 @@ class etherpad_lite (
 
   if $ensure == 'present' {
     mysql_grant{"${db_user}@${db_host}/${db_name}":
-      privileges => [ 'alter_priv', 'create_priv', 'select_priv', 'insert_priv',
-                      'update_priv', 'delete_priv', 'trigger_priv' ],
+      privileges => [ 'alter_priv',
+                      'create_priv',
+                      'select_priv',
+                      'insert_priv',
+                      'update_priv',
+                      'delete_priv',
+                      'trigger_priv' ],
       require => [ Mysql_database["${db_name}"], Mysql_user["${db_user}@${db_host}"] ];
     }
   }
 
-  group { "etherpad-lite":
+  group { 'etherpad-lite':
     ensure    => $ensure,
     allowdupe => false,
   }
 
-  user { "etherpad-lite":
+  user { 'etherpad-lite':
     ensure    => $ensure,
     allowdupe => false,
     gid       => 'etherpad-lite',
@@ -59,7 +73,7 @@ class etherpad_lite (
   }
 
   file { '/srv/etherpad-lite':
-    ensure => directory,
+    ensure   => directory,
     owner    => 'etherpad-lite',
     group    => 'etherpad-lite',
     require  => User['etherpad-lite'],
@@ -76,17 +90,19 @@ class etherpad_lite (
     notify   => Service['etherpad-lite'],
   }
 
-  file {
-    '/var/log/etherpad-lite':
+  file { '/var/log/etherpad-lite':
       ensure  => directory,
       owner   => 'etherpad-lite',
       group   => 'etherpad-lite',
       mode    => 0755,
       require => [ User['etherpad-lite'], Group['etherpad-lite'] ];
+  }
       
-    "/srv/etherpad-lite/settings.json":
+  file { '/srv/etherpad-lite/settings.json':
       ensure => $ensure,
-      mode => 0640, owner => etherpad-lite, group => etherpad-lite,
+      mode => 0640,
+      owner => etherpad-lite,
+      group => etherpad-lite,
       notify => Service["etherpad-lite"],
       require => Vcsrepo['/srv/etherpad-lite'];
   }
@@ -112,19 +128,20 @@ class etherpad_lite (
     }
   }
 
-  file {
-    "/etc/init.d/etherpad-lite":
-      source => "puppet:///modules/etherpad_lite/etherpad-lite.init",
-      owner => root, group => root, mode => 0755,
-      require => Vcsrepo['/srv/etherpad-lite'];
+  file { '/etc/init.d/etherpad-lite':
+      source   =>  'puppet:///modules/etherpad_lite/etherpad-lite.init',
+      owner    =>  'root',
+      group    =>  'root',
+      mode     =>  '0755',
+      require  =>  Vcsrepo['/srv/etherpad-lite'];
   }
 
   service { "etherpad-lite":
-    enable => true,
-    ensure => running,
+    enable     => true,
+    ensure     => running,
     hasrestart => true,
-    hasstatus => true,
-    require => [ File['/srv/etherpad-lite/settings.json', '/srv/etherpad-lite'], Mysql_grant["${db_user}@${db_host}/${db_name}" ] ]
+    hasstatus  => true,
+    require    => [ File['/srv/etherpad-lite/settings.json', '/srv/etherpad-lite'], Mysql_grant["${db_user}@${db_host}/${db_name}" ] ]
   }
 
   if $use_nagios {
@@ -133,25 +150,33 @@ class etherpad_lite (
   
   augeas {
     "logrotate_etherpad":
-      context => "/files/etc/logrotate.d/etherpad-lite/rule",
-      changes => [ 'set file /var/log/etherpad-lite/*.log', 'set rotate 5', 'set schedule daily',
-                   'set compress compress', 'set delaycompress delaycompress', 'set ifempty notifempty',
-                   'set copytruncate copytruncate', 'set create/mode 0640', 'set create/owner etherpad-lite',
-                   'set create/group etherpad-lite' ]
+      context   => "/files/etc/logrotate.d/etherpad-lite/rule",
+      changes   => [ 'set file /var/log/etherpad-lite/*.log',
+                     'set rotate 5',
+                     'set schedule daily',
+                     'set compress compress',
+                     'set delaycompress delaycompress',
+                     'set ifempty notifempty',
+                     'set copytruncate copytruncate',
+                     'set create/mode 0640',
+                     'set create/owner etherpad-lite',
+                     'set create/group etherpad-lite' ]
   }
 
   if $clear_pads != 'absent' {
 
-    file {
-      "/usr/local/bin/clear-old-pads.rb":
-        content => template("etherpad_lite/pad-ecology/clear-old-pads.erb"),
-        mode => 0755, owner => etherpad-lite, group => etherpad-lite;
+    file { '/usr/local/bin/clear-old-pads.rb':
+      content    => template("etherpad_lite/pad-ecology/clear-old-pads.erb"),
+      mode       => 0755,
+      owner      => etherpad-lite,
+      group => etherpad-lite;
     }
 
-    cron { "pad_ecology":
-      command => "/usr/local/bin/clear-old-pads.rb 2>/dev/null",
-      user => etherpad-lite,
-      hour => "$clear_pads", minute => 0;
+    cron { 'pad_ecology':
+      command    => '/usr/local/bin/clear-old-pads.rb 2>/dev/null',
+      user       => 'etherpad-lite',
+      hour       => "$clear_pads",
+      minute => 0;
     }
   }
 }
